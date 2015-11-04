5d2608b00fc95e0a0449cb573fcd798bb7aed42c
Quentin <quentin.decobert@gmail.com>
2015-11-04 11:37:56 +0100

    Login

--- src/Controller/CustomerController.php
+++ src/Controller/CustomerController.php
@@ -26,12 +26,12 @@ class CustomerController extends BaseController
 
     public function create()
     {
-            $param = json_decode($app->request->getBody());
-            $username = $param->username;
-            $lastname = $param->lastName;
-            $firstname =  $param->firstName;
-            $password = $param->password;
-            $email = $param->email;
+        $param = json_decode($app->request->getBody());
+        $username = $param->username;
+        $lastname = $param->lastName;
+        $firstname =  $param->firstName;
+        $password = $param->password;
+        $email = $param->email;
 
 
         $customer = new Customer();
@@ -43,5 +43,24 @@ class CustomerController extends BaseController
 
         $this->entityManager->persist($customer);
         $this->entityManager->flush();
+    }
+
+    public function login()
+    {
+        $username = !empty($_GET['username']) ? $_GET['username'] : null;
+        $password = !empty($_GET['password']) ? $_GET['password'] : null;
+
+        if (empty($username) || empty($password))
+            return [];
+
+        /** @var CustomerRepository $repo */
+        $repo = $this->entityManager->getRepository('App\\Entity\\Customer');
+
+        $count = (int) $repo->login($username, $password);
+
+        if ($count > 0)
+            return ['connected'];
+        else
+            return [];
     }
 }
